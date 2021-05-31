bc1f39fe55000eab6765d056160d5c66fa5d8276
SERGEY SELEZNEV <sergey_0218@mail.ru>
2021-05-31 12:18:04 +0700

    Add Book model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    book_title = models.CharField(
+        'Main Title', default='Book_Title', max_length=20, help_text="M_Title")
+
+    book_ISBN = models.CharField(
+        'Main Title', default='Book_ISBN', max_length=20, help_text="M_Title")
+
+    book_page_num = models.CharField(
+        'Main Title', default='Sheet_Page_Number', max_length=20, help_text="M_Title")
+
+    book_edition = models.CharField(
+        'Main Title', default='Book_Edition', max_length=20, help_text="M_Title")
+
+    book_weight = models.CharField(
+        'Main Title', default='Book_Weight', max_length=20, help_text="M_Title")
+
+    boook_count = models.IntegerField(
+        'Cost', default='Book_Count', max_length=25, help_text="Book Cost")
+
+    boook_cost = models.FloatField(
+        'Cost', default='Book_Cost', max_length=25, help_text="Book Cost")
+
+    pub_date = models.DateField('Pub date')
+
+    def __str__(self):
+
+        return self.book_title
