c2f0975b035dea0b4e0c8c08a7bbc29a87404852
SERGEY SELEZNEV <sergey_0218@mail.ru>
2021-05-31 07:39:27 +0700

    Add Search Page

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { Fragment, useEffect } from "react";
 import { SearchForm } from "../components/searchForm/SearchForm";
+import { BookList } from "../components/bookList/BookList";
+import { Loader } from "../components/loader/Loader";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchResponse,
+  selectResponseList,
+  selectSearchLoading,
+} from "../reduxToolkit/api/searchSlice";
+import { Header } from "../components/header/Header";
+import { Footer } from "../components/footer/Footer";
 
 export const Search = () => {
-  return <SearchForm />;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchResponse(""));
+  }, []);
+
+  const responseList = useSelector(selectResponseList);
+  const loading = useSelector(selectSearchLoading);
+
+  return (
+    <Fragment>
+      <Header />
+      <SearchForm />
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <BookList bookItems={responseList} />
+          <Footer />
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
