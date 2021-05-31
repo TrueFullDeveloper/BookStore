4a5c8a576c9846757d18c574f5f9e153e47d7020
SERGEY SELEZNEV <sergey_0218@mail.ru>
2021-06-01 03:04:07 +0700

    Modefy Models

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 
 class Publisher(models.Model):
     publisher_title = models.CharField(
-        'Publisher Title', default='Publisher_Title', max_length=20, )
+        'Название издательства', default='Publisher_Title', max_length=20, )
     publisher_phone = models.CharField(
-        'Publisher Phone', default='Publisher_Phone', max_length=20, )
+        'Номер издательства', default='Publisher_Phone', max_length=20, )
     publisher_address = models.CharField(
-        'Publisher Address', default='Publisher_Address', max_length=20, )
+        'Адрес издательства', default='Publisher_Address', max_length=20, )
 
     class Meta:
         verbose_name = 'Издательство'
@@ -21,11 +21,11 @@ class Publisher(models.Model):
 
 class Author(models.Model):
     author_first_name = models.CharField(
-        'Author First Name', default='NULL', max_length=20, )
+        'Имя автора', default='NULL', max_length=20, )
     author_second_name = models.CharField(
-        'Author Second Name', default='NULL', max_length=20, )
+        'Фамилия', default='NULL', max_length=20, )
     author_third_name = models.CharField(
-        'Author Third Name', default='NULL', max_length=20, )
+        'Отчество', default='NULL', max_length=20, )
 
     class Meta:
         verbose_name = 'Автор'
@@ -38,7 +38,7 @@ class Author(models.Model):
 
 class Genre(models.Model):
     genre_title = models.CharField(
-        'Genre Title', default='NULL', max_length=20, )
+        'Жанр', default='NULL', max_length=20, )
 
     class Meta:
         verbose_name = 'Жанр'
@@ -51,7 +51,7 @@ class Genre(models.Model):
 
 class Rating(models.Model):
     rating = models.IntegerField(
-        'Rating', default=0,)
+        'Оценка', default=0,)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
@@ -60,26 +60,26 @@ class Rating(models.Model):
 
     def __str__(self):
 
-        return 'Rating'
+        return 'Оценка'
 
 
 class Book(models.Model):
     book_title = models.CharField(
-        'Book Title', default='NULL', max_length=20, )
+        'Название книги', default='NULL', max_length=20, )
     book_ISBN = models.CharField(
-        'Book ISBN', default='NULL', max_length=20, )
+        'Номер ISBN', default='NULL', max_length=20, )
     book_page_num = models.CharField(
-        'Book Page Number', default='NULL', max_length=20, )
+        'Количество страниц', default='NULL', max_length=20, )
     book_edition = models.CharField(
-        'Book Edition', default='NULL', max_length=20, )
+        'Тираж', default='NULL', max_length=20, )
     book_weight = models.CharField(
-        'Book Weight', default='NULL', max_length=20, )
+        'Вес, г', default='NULL', max_length=20, )
     book_count = models.IntegerField(
-        'Book Count', default=0,)
+        'Количество книг', default=0,)
     book_cost = models.FloatField(
-        'Book Cost', default=0, )
-    is_available = models.BooleanField('Is Available', default=True)
-    pub_date = models.DateField('Pub date')
+        'Стоимость книги', default=0, )
+    is_available = models.BooleanField('Наличие', default=True)
+    pub_date = models.DateField('Дата публикации')
     author = models.ManyToManyField(Author)
     genre = models.ManyToManyField(Genre)
     rating = models.ManyToManyField(Rating)
@@ -97,11 +97,11 @@ class Book(models.Model):
 
 class DeliveryCompany(models.Model):
     delivery_company_title = models.CharField(
-        'Delivery Company Title', default='NULL', max_length=20, )
+        'Название Компании Доставки', default='NULL', max_length=20, )
     delivery_company_phone = models.CharField(
-        'Delivery Company Phone', default='NULL', max_length=20, )
+        'Номер Компании Доставки', default='NULL', max_length=20, )
     delivery_cost = models.FloatField(
-        'Delivery Cost', default=0,)
+        'Стоимость доставки', default=0,)
 
     class Meta:
         verbose_name = 'Доставщик'
@@ -114,9 +114,9 @@ class DeliveryCompany(models.Model):
 
 class Order(models.Model):
     order_number = models.CharField(
-        'Order Number', default='NULL', max_length=20, )
+        'Номер заказа', default='NULL', max_length=20, )
     total_cost = models.FloatField(
-        'Total Cost', default=0, )
+        'Общая Стоимость', default=0, )
     is_completed = models.BooleanField("Is Completed", default=False)
     completed_on = models.DateTimeField("Completed on", null=True, blank=True)
     created_at = models.DateTimeField("Created at", auto_now_add=True)
@@ -135,8 +135,8 @@ class Order(models.Model):
 
 class Feedback(models.Model):
     feedback_text = models.CharField(
-        'Feedback Text', default='NULL', max_length=3000, )
-    created_at = models.DateTimeField("Created at", auto_now_add=True)
+        'Отзыв', default='NULL', max_length=3000, )
+    created_at = models.DateTimeField("Оставленный", auto_now_add=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -146,12 +146,12 @@ class Feedback(models.Model):
 
     def __str__(self):
 
-        return self.created_at
+        return "Отзыв"
 
 
 class Basket(models.Model):
     basket_number = models.CharField(
-        'Basket Number', default='NULL', max_length=3000, )
+        'Номер Корзины', default='NULL', max_length=3000, )
     book = models.ManyToManyField(Book)
     creator = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -162,3 +162,17 @@ class Basket(models.Model):
     def __str__(self):
 
         return self.basket_number
+
+
+class Promo(models.Model):
+    promo_cod = models.CharField(
+        'Промо-код', default='NULL', max_length=3000, )
+    book = models.ManyToManyField(Book)
+
+    class Meta:
+        verbose_name = 'Промо-код'
+        verbose_name_plural = 'Промо-коды'
+
+    def __str__(self):
+
+        return self.promo_cod
